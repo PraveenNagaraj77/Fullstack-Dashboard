a14fbe655ee0e0a9378ba90d10d8311c63eea723
PraveenNagaraj77 <praveennagaraj76@gmail.com>
2025-02-04 18:18:27 +0530

    Initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className='bg-black'>Fullstack Dashboard</h1>
+    <Button>New Project</Button>
+    </>
+  )
+}
+
+export default App
